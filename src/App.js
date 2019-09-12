20ff67f971e83f3f5b9f46bc103787784138f45b
Sebastian <sebasaa@stud.ntnu.no>
2019-09-12 17:54:30 +0200

    Layout improvements

--- src/App.js
+++ src/App.js
@@ -12,11 +12,16 @@ function App() {
       <div className="sidebar">
         <h2>sidebar</h2>
       </div>
-      <div className="content1">
-        <h2>Content 1</h2>
-      </div>
-      <div className="content2">
-        <h2>Content 2</h2>
+      <div className="content">
+        <div className="svg-section">
+          <h2>SVG</h2>
+        </div>
+        <div className="text-section">
+          <h2>Text</h2>
+        </div>
+        <div className="sound-section">
+          <h2>Sound</h2>
+        </div>
       </div>
       <div className="footer">
         <h4>Footer</h4>
---
+++ src/components/buttonRow.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+
+class ButtonRow extends React.Component {
+    render() {
+    return 
+    <div className="buttons">
+        <div className="button1"><h2>Button 1</h2></div>
+        <div className="button2"><h2>Button 2</h2></div>
+        <div className="button3"><h2>Button 3</h2></div>
+    </div>
+    }
+}
