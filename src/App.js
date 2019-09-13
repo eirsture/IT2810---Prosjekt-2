489879166e784dea7ce328219cb7cad3d1262a56
eirsture <eirik.sture@gmail.com>
2019-09-13 14:30:38 +0200

    changed App.js to Class

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import Sidebar from './components/sidebarContent'
 import ButtonRow from './components/ButtonRow'
 import Display from './components/Display'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="header">
-        <h2>header</h2>
-      </header>
-      <div className="sidebar">
-          <Sidebar categories={["Cat", "Dog", "Horse"]} />
+class App extends React.Components {
+  render() {
+    return (
+      <div className="App">
+        <header className="header">
+          <h2>header</h2>
+        </header>
+        <div className="sidebar">
+            <Sidebar categories={["Cat", "Dog", "Horse"]} />
+        </div>
+        <div className="content">
+          <Display />
+          
+          <ButtonRow />
+        </div>
+        <div className="footer">
+          <h4>Footer</h4>
+        </div>
       </div>
-      <div className="content">
-        <Display />
-        
-        <ButtonRow/>
-      </div>
-      <div className="footer">
-        <h4>Footer</h4>
-      </div>
-    </div>
-  );
+    )
+  }
 }
 
 export default App;
