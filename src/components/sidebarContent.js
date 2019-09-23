a8fd34137b504cbb6b44c15fd462e587a7e6f633
eirsture <eirik.sture@gmail.com>
2019-09-23 15:13:54 +0200

    changed sidebarContent to stateless function, and changed indentation

--- src/components/sidebarContent.js
+++ src/components/sidebarContent.js
@@ -1,22 +1,32 @@
-import React from 'react';
-import SidebarSection from './sidebarSection';
+import React from 'react'
+import SidebarSection from './sidebarSection'
 
-class SidebarContent extends React.Component {
-    render() {
-        return(
-            <div className="wrapper">
-                <div className="Image">
-                    <SidebarSection title="Image" categories={this.props.categories} selectedCategory={this.props.selectedCategory}/>
-                </div>
-                <div className="Text">
-                    <SidebarSection title="Text" categories={this.props.categories} selectedCategory={this.props.selectedCategory}/>
-                </div>
-                <div className="Sound">
-                    <SidebarSection title="Sound" categories={this.props.categories} selectedCategory={this.props.selectedCategory}/>
-                </div>
-            </div>
-        )
-    }
+const SidebarContent = ({ categories, selectedCategory }) => {
+  return (
+    <div className="wrapper">
+      <div className="Image">
+        <SidebarSection
+          title="Image"
+          categories={categories}
+          selectedCategory={selectedCategory}
+        />
+      </div>
+      <div className="Text">
+        <SidebarSection
+          title="Text"
+          categories={categories}
+          selectedCategory={selectedCategory}
+        />
+      </div>
+      <div className="Sound">
+        <SidebarSection
+          title="Sound"
+          categories={categories}
+          selectedCategory={selectedCategory}
+        />
+      </div>
+    </div>
+  )
 }
 
-export default SidebarContent;
+export default SidebarContent
--- src/components/sidebarSection.js
+++ src/components/sidebarSection.js
@@ -1,55 +1,62 @@
-import React from 'react';
-import './sidebarSection.css';
+import React from 'react'
+import './sidebarSection.css'
 
 class SidebarSection extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            selectedCategory: this.props.categories[0]
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedCategory: this.props.categories[0]
     }
+  }
 
-    categoryChanged = (e) => {
-        this.setState({ selectedCategory: e.currentTarget.value})
-        this.props.selectedCategory(this.props.title, e.currentTarget.value)
-        this.saveToLocalStorage(e.currentTarget.value)
-    }
+  categoryChanged = e => {
+    this.setState({ selectedCategory: e.currentTarget.value })
+    this.props.selectedCategory(this.props.title, e.currentTarget.value)
+    this.saveToLocalStorage(e.currentTarget.value)
+  }
 
-    saveToLocalStorage = (category) => {
-        localStorage.setItem(this.props.title, category)
-    }
+  saveToLocalStorage = category => {
+    localStorage.setItem(this.props.title, category)
+  }
 
-    componentDidMount() {
-        const locallyStoredCategory = localStorage.getItem(this.props.title)
-        if (locallyStoredCategory) {
-            this.setState({ selectedCategory: locallyStoredCategory})
-        }
+  componentDidMount() {
+    const locallyStoredCategory = localStorage.getItem(this.props.title)
+    if (locallyStoredCategory) {
+      this.setState({ selectedCategory: locallyStoredCategory })
+    }
+  }
 
-    }
-    
-    render() {
-        return(
-            <div className="sectionContainer">
-                <h3>{this.props.title}</h3>
-                <form>
-                    {
-                        this.props.categories.map((category, index) => {
-                            return  <div key={index} className={this.state.selectedCategory === category ? "radio active" : "radio"}>
-                                        <label>
-                                        <input type="radio" 
-                                            value={category}
-                                            checked={this.state.selectedCategory === category}
-                                            onChange={this.categoryChanged}
-                                        />
-                                        {category}
-                                        </label>
-                                    </div>
-                        })
-                    }
-                </form>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="sectionContainer">
+        <h3>{this.props.title}</h3>
+        <form>
+          {this.props.categories.map((category, index) => {
+            return (
+              <div
+                key={index}
+                className={
+                  this.state.selectedCategory === category
+                    ? 'radio active'
+                    : 'radio'
+                }
+              >
+                <label>
+                  <input
+                    type="radio"
+                    value={category}
+                    checked={this.state.selectedCategory === category}
+                    onChange={this.categoryChanged}
+                  />
+                  {category}
+                </label>
+              </div>
+            )
+          })}
+        </form>
+      </div>
+    )
+  }
 }
 
-export default SidebarSection;
+export default SidebarSection
